d4949eaef9b47f0aa8b86a44b3200b9bc6017bea
mylovepooh <jun8414@gmail.com>
2022-05-06 12:44:34 +0900

    simple consumer

--- consumer/src/main/java/com/mylovepooh/kafka/consumer/SimpleConsumer.java
+++ consumer/src/main/java/com/mylovepooh/kafka/consumer/SimpleConsumer.java
@@ -1,10 +1,40 @@
 package com.mylovepooh.kafka.consumer;
 
-public class SimpleConsumer {
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class SimpleConsumer {
+	private static String TOPIC_NAME = "test";
+	private static String BOOTSTRAP_SERVERS = "192.168.75.184:9092,192.168.75.185:9092,192.168.75.186:9092";
+	private static String CONSUMER_GROUP = "TestGroupA";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello consumer");
+		Properties properties = new Properties();
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		properties.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP);
+		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+		consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+		try {
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+				for (ConsumerRecord<String, String> record : records)
+					System.out.println(record.value());
+			}
+		} finally {
+			consumer.close();
+		}
 	}
 
 }
